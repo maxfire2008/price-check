965fe0716ecc94b5385eb56dc0172945d2d5e19f
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2023-01-25 18:42:17 +1100

    price check now has wider range due to github actions slowness

--- price_check_check.py
+++ price_check_check.py
@@ -2,11 +2,11 @@ import datetime
 
 
 def price_check_check():
-    # between saturday 11:55am and 12:30pm UTC
+    # between saturday 11:00 and 14:00 UTC
     if (
         datetime.datetime.utcnow().weekday() == 5 and
-        datetime.datetime.utcnow().hour == 11 and
-        datetime.datetime.utcnow().minute >= 55
+        datetime.datetime.utcnow().hour >= 11 and
+        datetime.datetime.utcnow().hour <= 14
     ):
         return "Price check check passed"
     return "Price check check has run the every time response"
